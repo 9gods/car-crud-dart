5b08c7a486294757b7d5b6cff6c069a116beccef
9gods <codingcomelli@gmail.com>
2025-03-01 15:05:47 -0300

    feat: implements car service

---
+++ lib/services/car_service.dart
@@ -0,0 +1,66 @@
+import '../models/car.dart';
+import '../repositories/car_repository.dart';
+
+class CarService {
+  final CarRepository _repository;
+
+  CarService(this._repository);
+
+  List<Car> getCars() => _repository.getAll();
+  List<Car>? getCarsByBrand(String brand) => _repository.getByBrand(brand);
+  List<Car>? getCarsByYear(int year) => _repository.getByYear(year);
+
+  void createCar(
+    String brand,
+    String model,
+    String engine,
+    String engineCode,
+    int year,
+    int horsepower,
+    int price,
+    int weight,
+  ) {
+    final newCar = Car(
+      id: _repository.getAll().length + 1,
+      brand: brand,
+      model: model,
+      engine: engine,
+      engineCode: engineCode,
+      horsepower: horsepower,
+      price: price,
+      weight: weight,
+      year: year,
+    );
+    _repository.create(newCar);
+  }
+
+  bool updateCar(
+    int id,
+    String brand,
+    String model,
+    String engine,
+    String engineCode,
+    int year,
+    int horsepower,
+    int price,
+    int weight,
+  ) {
+    final existingCar = _repository.getById(id);
+    if (existingCar == null) return false;
+    final updatedCar = Car(
+      id: id,
+      brand: brand,
+      model: model,
+      engine: engine,
+      engineCode: engineCode,
+      horsepower: horsepower,
+      price: price,
+      weight: weight,
+      year: year,
+    );
+
+    return _repository.update(id, updatedCar);
+  }
+
+  bool deleteCar(int id) => _repository.delete(id);
+}
